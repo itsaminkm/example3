536464735ac1c387041f1c8f41e92ace96c08ede
itsaminkm <130796368+itsaminkm@users.noreply.github.com>
2023-05-26 18:32:45 +0330

    Add files via upload

---
+++ project1/project2/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace project2
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("pleas enter your number : ");
+            num = int.Parse(Console.ReadLine());
+            num *= 3;
+            Console.WriteLine(num);
+            Console.WriteLine("enter any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
